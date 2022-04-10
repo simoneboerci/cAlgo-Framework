f0bab94a9aece970e6022d0dc005c332bc2a21cf
Simone <boercisimone@gmail.com>
2022-04-10 15:55:24 +0200

    Unity Robot added

---
+++ cAlgo-Framework/cAlgo-Framework/Strategies/StrategyBase.cs
@@ -0,0 +1,22 @@
+﻿namespace cAlgoUnityFramework.Strategies
+{
+    public abstract class StrategyBase : IStrategy
+    {
+        #region Public Methods
+
+        public void Execute()
+        {
+            if (CanTrade()) LookForOpportunities();
+        }
+
+        #endregion
+
+        #region Protected Methods
+
+        protected abstract void LookForOpportunities();
+
+        protected virtual bool CanTrade() => true;
+
+        #endregion
+    }
+}
