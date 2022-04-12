b2c9a83d6b98b170f4906ebb0e653aa6c29ba2fd
Simone <boercisimone@gmail.com>
2022-04-12 07:28:31 +0200

    Strategy order execution shortcut

--- cAlgo-Framework/cAlgo-Framework/Strategies/StrategyBase.cs
+++ cAlgo-Framework/cAlgo-Framework/Strategies/StrategyBase.cs
@@ -7,6 +7,8 @@ namespace cAlgoUnityFramework.Strategies
 {
     public abstract class StrategyBase : IStrategy
     {
+        #region Variables
+
         #region Public Variables
 
         public Account? Account { get { return _unityRobot?.Account; } }
@@ -40,6 +42,8 @@ namespace cAlgoUnityFramework.Strategies
         #region Protected Variables
 
         protected UnityRobot? _unityRobot;
+
+        #endregion
 
         #endregion
 
@@ -81,6 +85,54 @@ namespace cAlgoUnityFramework.Strategies
 
             _unityRobot = null;       
         }
+
+        #endregion
+
+        #region Orders
+
+        #region Execute Market Orders
+
+        public TradeResult ExecuteBuyOrder() => _unityRobot.ExecuteBuyOrder(MarketData.SymbolName, PositionSizeModule.GetPositionSize(), _unityRobot.Name, StopLossModule.GetStopLossPips(), TakeProfitModule.GetTakeProfitPips());
+        public TradeResult ExecuteSellOrder() => _unityRobot.ExecuteSellOrder(MarketData.SymbolName, PositionSizeModule.GetPositionSize(), _unityRobot.Name, StopLossModule.GetStopLossPips(), TakeProfitModule.GetTakeProfitPips());
+
+        #endregion
+
+        #region Execute Buy Range Orders
+
+        public TradeResult ExecuteBuyRangeOrder(double marketRangePips, double basePrice) => _unityRobot.ExecuteBuyRangeOrder(MarketData.SymbolName, PositionSizeModule.GetPositionSize(), marketRangePips, basePrice, _unityRobot.Name, StopLossModule.GetStopLossPips(), TakeProfitModule.GetTakeProfitPips());
+        public TradeResult ExecuteSellRangeOrder(double marketRangePips, double basePrice) => _unityRobot.ExecuteSellRangeOrder(MarketData.SymbolName, PositionSizeModule.GetPositionSize(), marketRangePips, basePrice, _unityRobot.Name, StopLossModule.GetStopLossPips(), TakeProfitModule.GetTakeProfitPips());
+
+        #endregion
+
+        #region Place Limit Orders
+
+        public TradeResult PlaceBuyLimitOrder(double targetPrice) => _unityRobot.PlaceBuyLimitOrder(MarketData.SymbolName, PositionSizeModule.GetPositionSize(), targetPrice, _unityRobot.Name, StopLossModule.GetStopLossPips(), TakeProfitModule.GetTakeProfitPips());
+        public TradeResult PlaceBuyLimitOrder(double targetPrice, DateTime? expiration) => _unityRobot.PlaceBuyLimitOrder(MarketData.SymbolName, PositionSizeModule.GetPositionSize(), targetPrice, _unityRobot.Name, StopLossModule.GetStopLossPips(), TakeProfitModule.GetTakeProfitPips(), expiration);
+
+        public TradeResult PlaceSellLimitOrder(double targetPrice) => _unityRobot.PlaceSellLimitOrder(MarketData.SymbolName, PositionSizeModule.GetPositionSize(), targetPrice, _unityRobot.Name, StopLossModule.GetStopLossPips(), TakeProfitModule.GetTakeProfitPips());
+        public TradeResult PlaceSellLimitOrder(double targetPrice, DateTime? expiration) => _unityRobot.PlaceSellLimitOrder(MarketData.SymbolName, PositionSizeModule.GetPositionSize(), targetPrice, _unityRobot.Name, StopLossModule.GetStopLossPips(), TakeProfitModule.GetTakeProfitPips(), expiration);
+
+        #endregion
+
+        #region Place Stop-Limit Orders
+
+        public TradeResult PlaceBuyStopLimitOrder(double targetPrice, double stopLimitRangePips) => _unityRobot.PlaceBuyStopLimitOrder(MarketData.SymbolName, PositionSizeModule.GetPositionSize(), targetPrice, stopLimitRangePips, _unityRobot.Name, StopLossModule.GetStopLossPips(), TakeProfitModule.GetTakeProfitPips());
+        public TradeResult PlaceBuyStopLimitOrder(double targetPrice, double stopLimitRangePips, DateTime? expiration) => _unityRobot.PlaceBuyStopLimitOrder(MarketData.SymbolName, PositionSizeModule.GetPositionSize(), targetPrice, stopLimitRangePips, _unityRobot.Name, StopLossModule.GetStopLossPips(), TakeProfitModule.GetTakeProfitPips(), expiration);
+
+        public TradeResult PlaceSellStopLimitOrder(double targetPrice, double stopLimitRangePips) => _unityRobot.PlaceSellStopLimitOrder(MarketData.SymbolName, PositionSizeModule.GetPositionSize(), targetPrice, stopLimitRangePips, _unityRobot.Name, StopLossModule.GetStopLossPips(), TakeProfitModule.GetTakeProfitPips());
+        public TradeResult PlaceSellStopLimitOrder(double targetPrice, double stopLimitRangePips, DateTime? expiration) => _unityRobot.PlaceSellStopLimitOrder(MarketData.SymbolName, PositionSizeModule.GetPositionSize(), stopLimitRangePips, targetPrice, _unityRobot.Name, StopLossModule.GetStopLossPips(), TakeProfitModule.GetTakeProfitPips(), expiration);
+
+        #endregion
+
+        #region Place Stop Orders
+
+        public TradeResult PlaceBuyStopOrder(double targetPrice) => _unityRobot.PlaceBuyStopOrder(MarketData.SymbolName, PositionSizeModule.GetPositionSize(), targetPrice, _unityRobot.Name, StopLossModule.GetStopLossPips(), TakeProfitModule.GetTakeProfitPips());
+        public TradeResult PlaceBuyStopOrder(double targetPrice, DateTime? expiration) => _unityRobot.PlaceBuyStopOrder(MarketData.SymbolName, PositionSizeModule.GetPositionSize(), targetPrice, _unityRobot.Name, StopLossModule.GetStopLossPips(), TakeProfitModule.GetTakeProfitPips(), expiration);
+
+        public TradeResult PlaceSellStopOrder(double targetPrice) => _unityRobot.PlaceSellStopOrder(MarketData.SymbolName, PositionSizeModule.GetPositionSize(), targetPrice, _unityRobot.Name, StopLossModule.GetStopLossPips(), TakeProfitModule.GetTakeProfitPips());
+        public TradeResult PlaceSellStopOrder(double targetPrice, DateTime? expiration) => _unityRobot.PlaceSellStopOrder(MarketData.SymbolName, PositionSizeModule.GetPositionSize(), targetPrice, _unityRobot.Name, StopLossModule.GetStopLossPips(), TakeProfitModule.GetTakeProfitPips(), expiration);
+
+        #endregion
 
         #endregion
 
